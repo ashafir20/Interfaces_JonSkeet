b2f47cbf640e4526b0d1b838ea38658c8fd2853f
ashafir <aviv@optimalfinance.co.il>
2014-09-26 01:12:12 +0300

    added all code for this chapter

---
+++ Interfaces_JonSkeet/Licence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interfaces_JonSkeet
+{
+    public class Licence
+    {
+        private readonly DateTime expiry;
+
+        public Licence(DateTime expiry)
+        {
+            this.expiry = expiry;
+        }
+
+        //Makes testing very hard as DateTime.Now is a static method that
+        //cant be mocked or allow dependency injection
+        public bool hasExpired
+        {
+            get { return DateTime.UtcNow >= expiry; }
+        }
+    }
+}
---
+++ Tests/TestDates.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Interfaces_JonSkeet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NodaTime;
+using NodaTime.Testing;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestDates
+    {
+        [TestMethod]
+        public void ExpiredLicence()
+        {
+            Instant expiry = Instant.FromUtc(2000, 1, 1, 0, 0, 0);
+            FakeClock clock = new FakeClock(expiry + Duration.Epsilon);
+            ImprovedLicence licence = new ImprovedLicence(expiry, clock);
+            Assert.IsTrue(licence.hasExpired);
+        }
+
+
+        [TestMethod] 
+        public void NonExpiredLicence()
+        {
+            Instant expiry = Instant.FromUtc(2000, 1, 1, 0, 0, 0);
+            FakeClock clock = new FakeClock(expiry - Duration.Epsilon);
+            ImprovedLicence licence = new ImprovedLicence(expiry, clock);
+            Assert.IsFalse(licence.hasExpired);
+        }
+
+
+
+        [TestMethod]
+        public void ExpiredAtExactInstant()
+        {
+            Instant expiry = Instant.FromUtc(2000, 1, 1, 0, 0, 0);
+            FakeClock clock = new FakeClock(expiry);
+            ImprovedLicence licence = new ImprovedLicence(expiry, clock);
+            Assert.IsTrue(licence.hasExpired);
+        }
+
+        [TestMethod]
+        public void NonExpiredLicenceBecomesExpiredLicence()
+        {
+            Instant expiry = Instant.FromUtc(2000, 1, 1, 0, 0, 0);
+            FakeClock clock = new FakeClock(expiry - Duration.Epsilon);
+            ImprovedLicence licence = new ImprovedLicence(expiry, clock);
+
+            Assert.IsFalse(licence.hasExpired);
+
+            clock.AdvanceTicks(1);
+
+            Assert.IsTrue(licence.hasExpired);
+
+        }
+    }
+}
